b6c9e59ea924bc2e20195c70febf8596cafdad40
Sam Kumar <samkumar99@gmail.com>
2016-05-21 18:39:24 -0700

    Make the program verify keypairs

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+  if len(os.Args) != 3 {
+    fmt.Printf("Usage: %s <signing key> <verifying key>\n", os.Args[0])
+    os.Exit(0)
+  }
+  sk, e := UnFmtKey(os.Args[1])
+  if e != nil {
+    fmt.Printf("Could not unformat signing key: %v\n", e)
+    os.Exit(1)
+  }
+  vk, e := UnFmtKey(os.Args[2])
+  if e != nil {
+    fmt.Printf("Could not unformat verifying key: %v\n", e)
+    os.Exit(1)
+  }
+  if !CheckKeypair(sk, vk) {
+    fmt.Println("valid keypair failed to validate")
+  }
 }
